248729b47d7174d239bab9eb6668a386668f9e0e
Daamin909 <ashai.daamin@gmail.com>
2025-11-30 16:19:15 +0530

    added music, added items to village

--- scripts/corruptor_intro.gd
+++ scripts/corruptor_intro.gd
@@ -3,4 +3,4 @@ extends CharacterBody2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "player_playable_intro":
-		get_tree().change_scene_to_file("res://scenes/intro_cutscene.tscn")
+		get_tree().change_scene_to_file("res://scenes/home.tscn")
