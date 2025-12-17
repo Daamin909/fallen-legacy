86294577b3e186faf52deb678d3c094feceda4e4
Daamin909 <ashai.daamin@gmail.com>
2025-12-17 16:28:45 +0530

    Added fainted bedroom dialgoue scene

---
+++ scripts/doctor.gd
@@ -0,0 +1,76 @@
+extends CharacterBody2D
+
+@export var speed: float = 250.0
+@export var jump_velocity: float = -250.0
+
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+var target_position: Vector2 = Vector2.ZERO
+var moving: bool = false
+
+func _ready() -> void:
+	anim.play("idle_left")
+
+func move_to(pos: Vector2) -> void:
+	target_position = pos
+	moving = true
+
+
+func _physics_process(delta: float) -> void:
+	var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if not moving:
+		move_and_slide()
+		return
+
+	var dx: float = target_position.x - global_position.x
+	if abs(dx) <= 4.0:
+		velocity.x = 0
+		moving = false
+		_play_idle()
+		if is_on_floor():
+			global_position.x = target_position.x
+		move_and_slide()
+		return
+
+	var dir: float = sign(dx)
+	anim.flip_h = dir < 0
+	velocity.x = dir * speed
+	_play_run()
+
+	if is_on_floor() and should_jump(dir):
+		velocity.y = jump_velocity
+
+	move_and_slide()
+
+
+func should_jump(dir: float) -> bool:
+	var ss = get_world_2d().direct_space_state
+
+	var ground_from = global_position + Vector2(dir * 10, 4)
+	var ground_to = ground_from + Vector2(0, 28)
+	var ground_query = PhysicsRayQueryParameters2D.create(ground_from, ground_to)
+	ground_query.exclude = [self]
+	var ground_hit = ss.intersect_ray(ground_query)
+	if ground_hit == {}:
+		return true
+
+	var front_from = global_position + Vector2(dir * 10, -8)
+	var front_to = front_from + Vector2(dir * 18, -8)
+	var front_query = PhysicsRayQueryParameters2D.create(front_from, front_to)
+	front_query.exclude = [self]
+	var front_hit = ss.intersect_ray(front_query)
+	if not front_hit == {}:
+		return true
+
+	return false
+
+func _play_idle():
+	if anim.animation != "idle":
+		anim.play("idle")
+
+func _play_run():
+	if anim.animation != "run":
+		anim.play("run")
--- scripts/home.gd
+++ scripts/home.gd
@@ -6,14 +6,21 @@ var home_village = "res://scenes/home_village.tscn"
 const Balloon = preload("res://scenes/helpers/balloon/balloon.tscn")
 @export var dialogue_start: String = "start"
 
-
+@onready var doc := $doctor
+@onready var doc_sprite := $doctor/AnimatedSprite2D
+@onready var elder := $village_elder
+@onready var elder_sprite := $village_elder/AnimatedSprite2D
+var dialogue_num := 1
 
 func _ready():
+	elder_sprite.play("idle")
+	doc_sprite.play("idle_right")
 	if SceneManager.is_player_position_inside_home_right:
-		#player.set_physics_process(false)
+		player.set_physics_process(false)
 		player_sprite.flip_h = true
+		player.rotation_degrees = 90
 		SceneManager.is_player_position_inside_home_right = false
-		#_do_another_thingy()
+		_do_another_thingy()
 	else:
 		call_deferred("_set_initial_position")
 	pass
@@ -35,7 +42,30 @@ func do_scene_change() -> void:
 
 
 func _do_another_thingy() -> void:
-	#DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
-	var balloon: Node = Balloon.instantiate()
+	await _play_dialogue("res://dialogues/faint_1.dialogue")
+	doc.move_to(Vector2(69, -16))
+	await _play_dialogue("res://dialogues/faint_1.dialogue")
+
+
+	
+signal dialogue_finished
+
+func _play_dialogue(path: String) -> void:
+	var balloon := Balloon.instantiate()
 	get_parent().call_deferred("add_child", balloon)
-	balloon.call_deferred("start", load("res://dialogues/faint.dialogue"), dialogue_start)
+	await balloon.ready
+
+	balloon.start(load(path), dialogue_start)
+
+
+	if not DialogueManager.dialogue_ended.is_connected(_on_temp_dialogue_end):
+		DialogueManager.dialogue_ended.connect(_on_temp_dialogue_end)
+
+	balloon.start(load(path), dialogue_start)
+
+	await self.dialogue_finished
+
+
+func _on_temp_dialogue_end(_resource) -> void:
+	DialogueManager.dialogue_ended.disconnect(_on_temp_dialogue_end)
+	emit_signal("dialogue_finished")
