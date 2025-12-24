ecb2838523ddd22fc659f0569d3a9559d5b957aa
Daamin909 <ashai.daamin@gmail.com>
2025-12-25 00:30:34 +0530

    more to the graveyard

---
+++ scripts/graveyard.gd
@@ -0,0 +1,51 @@
+extends Node2D
+const Balloon = preload("res://scenes/helpers/balloon/balloon.tscn")
+@export var dialogue_start: String = "start"
+@onready var player := $Player
+@onready var player_sprite := $Player/AnimatedSprite2D
+@onready var master := $master
+
+var soul: Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	master.visible = false
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_detect_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		_do_thingy()
+ 
+func _do_thingy() -> void:
+	DialogueManager.dialogue_ended.connect(_on_end)
+	player_sprite.play("idle")
+	player.set_physics_process(false)
+	var balloon: Node = Balloon.instantiate()
+	get_parent().call_deferred("add_child", balloon)
+	balloon.call_deferred("start", load("res://dialogues/arkblade_reveal_soul.dialogue"), dialogue_start)
+	
+
+	spawn_soul()
+	
+	
+func spawn_soul() -> void:
+	soul = master.duplicate(true)
+	soul.modulate = Color(1, 1, 1, 0.3)
+	var sprite := soul.get_node("AnimatedSprite2D") as AnimatedSprite2D
+	sprite.play("death")
+	soul.position = master.position
+	soul.visible = true
+	get_parent().call_deferred("add_child", soul)
+	master.call_deferred("queue_free")
+
+
+func _on_end(_resource) -> void:
+	get_tree().queue_delete(soul)
+	DialogueManager.dialogue_ended.disconnect(_on_end)
