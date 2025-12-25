1758334ee09e9f41d7937f466692a64e17cc0f13
Daamin909 <ashai.daamin@gmail.com>
2025-12-25 12:55:45 +0530

    added 2 dungeons

--- scripts/graveyard.gd
+++ scripts/graveyard.gd
@@ -7,18 +7,13 @@ const Balloon = preload("res://scenes/helpers/balloon/balloon.tscn")
 
 var soul: Node2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	master.visible = false
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
-
-
+	
 func _on_detect_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		_do_thingy()
@@ -32,7 +27,6 @@ func _do_thingy() -> void:
 	balloon.call_deferred("start", load("res://dialogues/arkblade_reveal_soul.dialogue"), dialogue_start)
 	spawn_soul()
 	
-	
 func spawn_soul() -> void:
 	soul = master.duplicate(true)
 	soul.modulate = Color(1, 1, 1, 0.3)
@@ -43,15 +37,12 @@ func spawn_soul() -> void:
 	get_parent().call_deferred("add_child", soul)
 	master.call_deferred("queue_free")
 
-
 func _on_end(_resource) -> void:
 	get_tree().queue_delete(soul)
 	PopupManager.show_popup("Quest Added: Find the ArkBlade", 4, 305)
 	player.set_physics_process(true)
 	DialogueManager.dialogue_ended.disconnect(_on_end)
 
-
 func _on_detect_end_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		SceneManager.change_scene("res://scenes/dungeons.tscn")
- # Replace with function body.
+		SceneManager.change_scene("res://scenes/dungeon_1.tscn")
