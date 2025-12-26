84c0f8814441d9c06172d757073674f7bd0a1a0f
Daamin909 <ashai.daamin@gmail.com>
2025-12-26 11:53:51 +0530

    thing

--- scripts/fields.gd
+++ scripts/fields.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		SceneManager.change_scene("res://scenes/corruptor_hq.tscn") # Replace with function body.
+		SceneManager.change_scene("res://scenes/another_field.tscn") 
